1d920740205b7bad7ccad8de748385fff8a91685
IosifG <77391293+IosifG1@users.noreply.github.com>
2023-06-13 21:05:24 +0300

    commit

--- drive_app.c
+++ drive_app.c
@@ -17,6 +17,14 @@ GtkWidget *listView; // New list view widget
 
 char *message;
 
+typedef struct FileData {
+    char name[256];
+    char owner[256];
+    char dateCreated[256];
+    char size[256];
+} FileData;
+
+
 json_t *load_json_from_file(const char *filename)
 {
     json_error_t error;
@@ -96,64 +104,234 @@ int write_json_to_file(const char *filename, json_t *root)
     return success == 0;
 }
 
-void addToList(const char *item)
-{
+void addToList(const char *item, const char *owner, const char *dateCreated, const char *size) {
     GtkListStore *store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(listView)));
 
     GtkTreeIter iter;
     gtk_list_store_append(store, &iter);
-    gtk_list_store_set(store, &iter, 0, item, -1);
+    gtk_list_store_set(store, &iter, 0, item, 1, owner, 2, dateCreated, 3, size, -1);
 }
 
+int copyFile(const char *srcPath, const char *dstPath)
+{
+    FILE *srcFile = fopen(srcPath, "rb");
+    if (srcFile == NULL)
+    {
+        printf("Error opening source file for reading.\n");
+        return 0;
+    }
+
+    FILE *dstFile = fopen(dstPath, "wb");
+    if (dstFile == NULL)
+    {
+        printf("Error opening destination file for writing.\n");
+        fclose(srcFile);
+        return 0;
+    }
+
+    char buffer[1024];
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), srcFile)) > 0)
+    {
+        size_t bytesWritten = fwrite(buffer, 1, bytesRead, dstFile);
+        if (bytesWritten < bytesRead)
+        {
+            printf("Error writing to destination file.\n");
+            fclose(srcFile);
+            fclose(dstFile);
+            return 0;
+        }
+    }
+
+    fclose(srcFile);
+    fclose(dstFile);
+    return 1;
+}
+
+GtkWidget *listView;
+GtkWidget *popupMenu;
+
+gchar *selectedFileName = NULL;
+// Callback function for the copy menu item
+void copyFileCallback(GtkWidget *widget, gpointer data)
+{
+    // Create the source and destination file paths
+    gchar *srcPath = g_strdup_printf("./drive/%s", selectedFileName);
+    gchar *dstPath = g_strdup_printf("./copied_drive/%s", selectedFileName);
+
+    FILE *srcFile = fopen(srcPath, "rb");
+    if (srcFile == NULL)
+    {
+        g_print("Error opening source file for reading.\n");
+        g_free(srcPath);
+        g_free(dstPath);
+        return;
+    }
+
+    FILE *dstFile = fopen(dstPath, "wb");
+    if (dstFile == NULL)
+    {
+        g_print("Error opening destination file for writing.\n");
+        fclose(srcFile);
+        g_free(srcPath);
+        g_free(dstPath);
+        return;
+    }
+
+    char buffer[1024];
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), srcFile)) > 0)
+    {
+        size_t bytesWritten = fwrite(buffer, 1, bytesRead, dstFile);
+        if (bytesWritten < bytesRead)
+        {
+            g_print("Error writing to destination file.\n");
+            fclose(srcFile);
+            fclose(dstFile);
+            g_free(srcPath);
+            g_free(dstPath);
+            return;
+        }
+    }
+
+    fclose(srcFile);
+    fclose(dstFile);
+    g_print("File '%s' copied to '%s'\n", srcPath, dstPath);
+    g_free(srcPath);
+    g_free(dstPath);
+}
+
+// Callback function for the delete menu item
+void deleteFileCallback(GtkWidget *widget, gpointer data)
+{
+    // Check if a file is selected
+    if (selectedFileName == NULL) {
+        printf("No file selected.\n");
+        return;
+    }
+
+    // Create the file path
+    char* filePath = g_strdup_printf("./drive/%s", selectedFileName);
+
+    // Delete the selected file
+    if (remove(filePath) == 0)
+        printf("File deleted successfully.\n");
+    else
+        printf("Failed to delete the file.\n");
+
+    g_free(filePath);
+}
+// Create the popup menu
+void createPopupMenu()
+{
+    popupMenu = gtk_menu_new();
+
+    // Copy menu item
+    GtkWidget *copyMenuItem = gtk_menu_item_new_with_label("Copy");
+    g_signal_connect(G_OBJECT(copyMenuItem), "activate", G_CALLBACK(copyFileCallback), NULL);
+    gtk_menu_shell_append(GTK_MENU_SHELL(popupMenu), copyMenuItem);
+
+    // Delete menu item
+    GtkWidget *deleteMenuItem = gtk_menu_item_new_with_label("Delete");
+    g_signal_connect(G_OBJECT(deleteMenuItem), "activate", G_CALLBACK(deleteFileCallback), NULL);
+    gtk_menu_shell_append(GTK_MENU_SHELL(popupMenu), deleteMenuItem);
+
+    gtk_widget_show_all(popupMenu);
+}
+
+// Callback function for the popup menu
+gboolean popupMenuCallback(GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+    if (event->type == GDK_BUTTON_PRESS) {
+        GdkEventButton *buttonEvent = (GdkEventButton *)event;
+        if (buttonEvent->button == GDK_BUTTON_SECONDARY) {
+            // Show the popup menu at the pointer position
+            gtk_menu_popup(GTK_MENU(popupMenu), NULL, NULL, NULL, NULL, buttonEvent->button, buttonEvent->time);
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+// Callback function for the selection changed event
 void selectionChanged(GtkTreeSelection *selection, gpointer data)
 {
     GtkTreeIter iter;
     GtkTreeModel *model;
-    if (gtk_tree_selection_get_selected(selection, &model, &iter))
-    {
-
+    if (gtk_tree_selection_get_selected(selection, &model, &iter)) {
         gchar *item;
         gtk_tree_model_get(model, &iter, 0, &item, -1);
         g_print("Selected File: %s\n", item);
 
-        int sock = connect_to_server();
-        if (sock == -1)
-        {
-            printf("Failed to connect to the server\n");
-            exit(EXIT_FAILURE);
-        }
-
-        send_message(sock, item);
-
-        receive_message(sock);
+        // Store the selected file name
+        if (selectedFileName != NULL)
+            g_free(selectedFileName);
+        selectedFileName = g_strdup(item);
 
         g_free(item);
     }
 }
 
+// Create the main application window
 void createMainApplicationWindow()
 {
     GtkWidget *mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(mainWindow), "Drive Application");
     gtk_window_set_default_size(GTK_WINDOW(mainWindow), 800, 600);
 
-    GtkWidget *scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
-    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledWindow),
-                                   GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-
-    GtkListStore *store = gtk_list_store_new(1, G_TYPE_STRING);
+    
+    GtkListStore *store = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
 
     listView = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
     g_object_unref(store);
 
     GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
-    GtkTreeViewColumn *column = gtk_tree_view_column_new_with_attributes("Files", renderer, "text", 0);
-    gtk_tree_view_append_column(GTK_TREE_VIEW(listView), column);
 
+    GtkTreeViewColumn *column = gtk_tree_view_column_new_with_attributes("Files",
+                                                                         renderer, "text", 0, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(listView), column);
+    gtk_tree_view_column_set_sizing(column, GTK_TREE_VIEW_COLUMN_FIXED);
+    gtk_tree_view_column_set_fixed_width(column, 200); // Set a fixed width for the column
+
+    column = gtk_tree_view_column_new_with_attributes("Owner",
+                                                      renderer, "text", 1, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(listView), column);
+    gtk_tree_view_column_set_sizing(column, GTK_TREE_VIEW_COLUMN_FIXED);
+    gtk_tree_view_column_set_fixed_width(column, 200); // Set a fixed width for the column
+
+    column = gtk_tree_view_column_new_with_attributes("Date created",
+                                                      renderer, "text", 2, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(listView), column);
+    gtk_tree_view_column_set_sizing(column, GTK_TREE_VIEW_COLUMN_FIXED);
+    gtk_tree_view_column_set_fixed_width(column, 200); // Set a fixed width for the column
+
+    column = gtk_tree_view_column_new_with_attributes("Size",
+                                                      renderer, "text", 3, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(listView), column);
+    gtk_tree_view_column_set_sizing(column, GTK_TREE_VIEW_COLUMN_FIXED);
+    gtk_tree_view_column_set_fixed_width(column, 200); // Set a fixed width for the column
+
+    // Create the tree selection and set the selection changed callback
+    GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(listView));
+    g_signal_connect(G_OBJECT(selection), "changed", G_CALLBACK(selectionChanged), NULL);
+
+    
+    // Set the right-click popup menu callback
+    g_signal_connect(G_OBJECT(listView), "button-press-event", G_CALLBACK(popupMenuCallback), NULL);
+
+    // Add the tree view to a scrolled window
+    GtkWidget *scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledWindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_container_add(GTK_CONTAINER(scrolledWindow), listView);
+
+    // Add the scrolled window to the main window
     gtk_container_add(GTK_CONTAINER(mainWindow), scrolledWindow);
 
+    // Show all the widgets
     gtk_widget_show_all(mainWindow);
+
+    // Create the popup menu
+    createPopupMenu();
 }
 
 void loginButtonClicked(GtkWidget *button, gpointer data)
@@ -215,11 +393,32 @@ void loginButtonClicked(GtkWidget *button, gpointer data)
             {
                 if (entry->d_type == DT_REG)
                 { // Check if it's a regular file
-                    printf("%s\n", entry->d_name);
-                    addToList(entry->d_name);
+                    char filePath[256];
+                    snprintf(filePath, sizeof(filePath), "./drive/%s", entry->d_name);
+
+                    struct stat fileStat;
+                    if (stat(filePath, &fileStat) == 0)
+                    {
+                        
+                        char dateCreated[256] = "Unknown";
+                        char size[256] = "Unknown";
+
+                        // Extract owner information
+                        struct passwd *pw = getpwuid(fileStat.st_uid);
+                        
+                            
+
+                        // Extract date created information
+                        struct tm *t = localtime(&fileStat.st_ctime);
+                        strftime(dateCreated, sizeof(dateCreated), "%Y-%m-%d %H:%M:%S", t);
+
+                        // Extract size information
+                        snprintf(size, sizeof(size), "%lld bytes", (long long)fileStat.st_size);
+
+                        addToList(entry->d_name, username, dateCreated, size);
+                    }
                 }
             }
-
             closedir(dir);
 
             // Get the selection object for the list view
